5a87cad29cee22c521e429eaf15d4770bf7dc856
T6509-0 <j30delossantos@gmail.com>
2024-12-07 14:46:35 +0800

    Update randomReact.js

--- script/event/randomReact.js
+++ script/event/randomReact.js
@@ -5,13 +5,13 @@ const request = require('request');
 
 module.exports.config = {
   name: "randomReact",
-  version: "GeoDevz69",
+  version: "Juno",
   credits: "cttro",
 };
 
 module.exports.handleEvent = async function ({ api, event }) {
   if (event.body) {
-    const emojis = ['😘', '🥺', '😀', '😾', '😛', '😽', '😸','🤭','😭','🙄','💋','😓','☹️','😒','🤧','🎀','😱','😑','😊','😞','🖤','🤖','😆'];
+    const emojis = ['🤍'];
     const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
     api.setMessageReaction(randomEmoji, event.messageID, () => {}, true);
