d12c08af040e375ebe2a908c44efa84283b2ac4b
T6509-0 <j30delossantos@gmail.com>
2024-12-11 12:55:45 +0800

    Update tik.js

--- script/tik.js
+++ script/tik.js
@@ -35,7 +35,7 @@ module.exports.run = async function({ api, event, args }) {
 		const videoData = videos[0];
 		const videoUrl = videoData.play;
 
-		const message = `Tiktok result:\n\n𝐏𝐨𝐬𝐭 𝐛𝐲: ${videoData.author.nickname}\nUsername: ${videoData.author.unique_id}\n\nTitle: ${videoData.title}`;
+		const message = `Here is your TikTok video:\n\nPost by: ${videoData.author.nickname}\nUsername: ${videoData.author.unique_id}\n\nTitle: ${videoData.title}`;
 
 		const filePath = path.join(__dirname, `/cache/tiktok_video.mp4`);
 		const writer = fs.createWriteStream(filePath);
